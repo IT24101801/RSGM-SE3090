e1c891e5af7f1c20ba2163a9f32ca29febecd3e1
IT24101801 <IT24101801@my.sliit.lk>
2026-09-11 12:37:42 +0530

    Create ApplicationUser

---
+++ backend/RSGM.Api/Models/Entities/ApplicationUser.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace RSGM.Api.Models.Entities;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public string FullName { get; set; } = string.Empty;
+
+    public bool IsActive { get; set; } = true;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; set; }
+}
