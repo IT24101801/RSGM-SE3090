caa64c2672fdeb3f263a5ff21b4b2f10c7242482
IT24101801 <IT24101801@my.sliit.lk>
2026-09-14 09:55:53 +0530

    feat: configure job seeker profile entity in application db context

--- backend/RSGM.Api/Data/ApplicationDbContext.cs
+++ backend/RSGM.Api/Data/ApplicationDbContext.cs
@@ -5,22 +5,18 @@ using RSGM.Api.Models.Entities;
 
 namespace RSGM.Api.Data;
 
-public class ApplicationDbContext
-    : IdentityDbContext<
-        ApplicationUser,
-        IdentityRole<Guid>,
-        Guid>
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
 {
-    public ApplicationDbContext(
-        DbContextOptions<ApplicationDbContext> options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
     }
 
     public DbSet<Skill> Skills => Set<Skill>();
 
-    protected override void OnModelCreating(
-        ModelBuilder builder)
+    public DbSet<JobSeekerProfile> JobSeekerProfiles => Set<JobSeekerProfile>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
@@ -41,6 +37,28 @@ public class ApplicationDbContext
 
             entity.HasIndex(x => x.NormalizedName)
                 .IsUnique();
+        });
+
+        builder.Entity<JobSeekerProfile>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+
+            entity.Property(x => x.Headline)
+                .HasMaxLength(150);
+
+            entity.Property(x => x.Location)
+                .HasMaxLength(150);
+
+            entity.Property(x => x.Bio)
+                .HasMaxLength(1000);
+
+            entity.HasIndex(x => x.UserId)
+                .IsUnique();
+
+            entity.HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
         });
     }
 }
